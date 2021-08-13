29a08c5bb24404ee372de3e5ead1acfad4bfaf0e
JonathanFrey0000 <82041401+JonathanFrey0000@users.noreply.github.com>
2021-08-13 04:59:43 -0700

    first commit

--- src/main/java/com/crumbs/PaymentService/controller/PaymentController.java
+++ src/main/java/com/crumbs/PaymentService/controller/PaymentController.java
@@ -1,13 +1,32 @@
 package com.crumbs.PaymentService.controller;
 
+import com.crumbs.PaymentService.dto.CreatePayment;
+import com.crumbs.PaymentService.dto.CreatePaymentResponse;
+import com.stripe.exception.StripeException;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
+import com.stripe.Stripe;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+
 
 @RestController
 public class PaymentController {
+    private static Gson gson = new Gson();
 
-    @PostMapping
-    public void createPaymentIntent(){
+    @PostMapping("/create-payment-intent")
+    public CreatePaymentResponse createPaymentIntent(@RequestBody CreatePayment createPayment) throws StripeException {
+        Stripe.apiKey = "sk_test_51JNmSeBoRXU1dvNXj8tijwpwJzNjk5kiSDNGbTvadbU2fuXKYvGgPzINOF1RUmmqh15uq3HcXXhNeGTrsG1h8FmL00S1Et9dFu";
 
+        PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
+                .setCurrency("usd")
+                .setAmount(15 * 100L) //createPayment use here
+                .build();
+
+        // Create a PaymentIntent with the order amount and currency
+        PaymentIntent intent = PaymentIntent.create(createParams);
+        return new CreatePaymentResponse(intent.getClientSecret());
     }
 }
--- src/main/java/com/crumbs/PaymentService/dto/CreatePaymentResponse.java
+++ src/main/java/com/crumbs/PaymentService/dto/CreatePaymentResponse.java
@@ -1,6 +1,6 @@
 package com.crumbs.PaymentService.dto;
 
-class CreatePaymentResponse {
+public class CreatePaymentResponse {
     private String clientSecret;
     public CreatePaymentResponse(String clientSecret) {
         this.clientSecret = clientSecret;
