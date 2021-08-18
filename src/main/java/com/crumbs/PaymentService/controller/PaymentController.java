26f446e4145979cad09bd1293a5e746f53abf0b7
JonathanFrey0000 <82041401+JonathanFrey0000@users.noreply.github.com>
2021-08-18 14:44:23 -0700

    .

--- src/main/java/com/crumbs/PaymentService/controller/PaymentController.java
+++ src/main/java/com/crumbs/PaymentService/controller/PaymentController.java
@@ -2,7 +2,9 @@ package com.crumbs.PaymentService.controller;
 
 import com.crumbs.PaymentService.dto.CreatePayment;
 import com.crumbs.PaymentService.dto.CreatePaymentResponse;
+import com.crumbs.PaymentService.dto.OrderTotal;
 import com.stripe.exception.StripeException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,20 +15,22 @@ import com.stripe.param.PaymentIntentCreateParams;
 
 
 @RestController
+@CrossOrigin
 public class PaymentController {
     private static Gson gson = new Gson();
 
     @PostMapping("/create-payment-intent")
-    public CreatePaymentResponse createPaymentIntent(@RequestBody CreatePayment createPayment) throws StripeException {
-        Stripe.apiKey = "sk_test_51JNmSeBoRXU1dvNXj8tijwpwJzNjk5kiSDNGbTvadbU2fuXKYvGgPzINOF1RUmmqh15uq3HcXXhNeGTrsG1h8FmL00S1Et9dFu";
+    public String createPaymentIntent(@RequestBody Float orderTotal) throws StripeException {
+       Stripe.apiKey = "sk_test_51JNmSeBoRXU1dvNXj8tijwpwJzNjk5kiSDNGbTvadbU2fuXKYvGgPzINOF1RUmmqh15uq3HcXXhNeGTrsG1h8FmL00S1Et9dFu";
 
         PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
                 .setCurrency("usd")
-                .setAmount(15 * 100L) //createPayment use here
+                .setAmount((long) (orderTotal * 100L)) //createPayment use here
                 .build();
 
         // Create a PaymentIntent with the order amount and currency
         PaymentIntent intent = PaymentIntent.create(createParams);
-        return new CreatePaymentResponse(intent.getClientSecret());
+        CreatePaymentResponse paymentResponse = new CreatePaymentResponse(intent.getClientSecret());
+        return gson.toJson(paymentResponse);
     }
 }
---
+++ src/main/java/com/crumbs/PaymentService/dto/OrderTotal.java
@@ -0,0 +1,9 @@
+package com.crumbs.PaymentService.dto;
+
+public class OrderTotal {
+    Long orderTotal;
+
+    public Long getOrderTotal() {
+        return orderTotal;
+    }
+}
